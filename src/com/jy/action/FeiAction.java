908ed56cb3906e176828e3e23d476a8222e50424
Administrator <Administrator@1RV4LDK2FHZD2N2>
2017-03-13 11:21:50 +0800

    main
    Change-Id: I2c0e37925fc3d138b63e6581b6fbef9e4a309fda
    Signed-off-by: zhufei <Administrator@1RV4LDK2FHZD2N2>

--- src/com/jy/action/FeiAction.java
+++ src/com/jy/action/FeiAction.java
@@ -2,4 +2,8 @@ package com.jy.action;
 
 public class FeiAction {
 
+	public static void main(String[] args) {
+		System.out.println("ÄãºÃ£¡");
+		System.out.println("Hello Word!!!");
+	}
 }
--- src/com/jy/model/Fei.java
+++ src/com/jy/model/Fei.java
@@ -2,4 +2,14 @@ package com.jy.model;
 
 public class Fei {
 
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 }
